4fefcc49c63fcbddd323f84d9523e460bc518910
abhi204 <abhi.chaurasia204@gmail.com>
2020-12-03 17:17:08 +0530

    Create DownloadStatus Model

--- download_app/models.py
+++ download_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class DownloadStatus(models.Model):
+    user_id = models.TextField()
+    table_name = models.TextField()
+    task_completed = models.BooleanField(default=False)
+    filename = models.TextField()
